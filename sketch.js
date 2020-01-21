70208f3b8373f9be714c829a68f429639985816f
unknown <PCC User@cama223m04.ad.pcc.edu>
2020-01-21 11:36:04 -0800

    lost of changes

--- sketch.js
+++ sketch.js
@@ -1,16 +1,46 @@
 function setup() {
   // put setup code here
-  createCanvas(500,500,WEBGL);
-   background(255,67,45);
+  createCanvas(800,700);
+  background(255,67,45);
+ 
+ push();
+ beginShape();
+    noStroke();
+	translate(0,60);
+  	vertex(197, 220);
+  	quadraticVertex(234, 280, 81, 371);
+  	bezierVertex(276, 263, 318, 187, 459, 93);
+endShape();
+pop();
+
+push();
+	fill(255,67,45);
+	noStroke();
+	translate(199,222);
+	circle(30, 40, 55);
+pop();
+
+pop();
+beginShape();
+	fill(255,67,45);
+	noStroke();
+	translate(-1, 105);
+  vertex(189, 159);
+  vertex(230, 177);
+  vertex(178, 201);
+endShape();
+pop();
+//beginShape();
+	//vertex(267, 168);
+	//quadraticVertex(252, 220, 177, 250);
+	//bezierVertex(296, 201, 335, 134, 427, 100);
+//endShape();
+
 
 }
 
 function draw() {
   // put drawing code here
-  //background(255,67,45);
-   rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  lights(50);
-  fill(245, 255, 48);
-  box(50, mouseX, mouseY);
+ 
+ 
 }
